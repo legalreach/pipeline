1e89144adaa24b47aa342c46eb421bc982cc8c5f
Avlok Kohli <avlok.kohli@gmail.com>
2012-05-23 20:08:46 -0700

    js rules by andy

--- app/controllers/pipeline_action_controller_extension.rb
+++ app/controllers/pipeline_action_controller_extension.rb
@@ -1,23 +1,28 @@
 module PipelineActionControllerExtension
- 	def self.included(base)
-		base.send(:include, InstanceMethods) 
-		#base.before_filter :my_method_1
-		#base.after_filter :my_method_2
- 	end
+    def self.included(base)
+        base.send(:include, InstanceMethods) 
+        #base.before_filter :my_method_1
+        #base.after_filter :my_method_2
+    end
 
-	module InstanceMethods
-		def render_for_pipeline(partial, locals)
-			if !partial.blank?
-			  rendered_view = render_to_string(:partial => partial, :locals => locals[:locals])
-			  rendered_view = rendered_view.gsub('"','\"').gsub(/[']/, '\\\\\'').gsub("//<![CDATA[", "").gsub("//]]>", "")
-			end
+    module InstanceMethods
+        
+        PIPELINE_JS_ESCAPE_MAP          = { '\\' => '\\\\', '</' => '<\/', "\r\n" => '\n', "\n" => '\n', "\r" => '\n', '"' => '\\"', "'" => "\\'" }
+        PIPELINE_JS_SECOND_ESCAPE_MAP   = {  "\n" => ' ', "\r" => ' ', '\t' => ' ' }
 
-			onload = ""
-			onload = yield rendered_view
+        def render_for_pipeline(partial, locals)
+            if !partial.blank?
+              rendered_view = render_to_string(:partial => partial, :locals => locals[:locals])
+              rendered_view = rendered_view.gsub(/\\|<\/|\r\n|[\n\r\"\']/) {|match| PIPELINE_JS_ESCAPE_MAP[match] }
+            else
+              rendered_view = ""
+            end
 
-			render :json => {
-			  :onload => onload.gsub(/[\n]+/, "").gsub(/[\t]+/,"")
-			}
-		end
-	end
+            onload = yield rendered_view
+
+            render :json => {
+                :onload => onload.gsub(/[\n\t\r]/) {|match| PIPELINE_JS_SECOND_ESCAPE_MAP[match] }
+            }
+        end
+    end
 end
